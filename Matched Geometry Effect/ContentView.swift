98d23468e9d1b95ea130a66a023fc664e8849536
med <theoutsider009@gmail.com>
2021-10-17 15:51:46 +0200

    Initial Commit

---
+++ Matched Geometry Effect/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Matched Geometry Effect
+//
+//  Created by Adam Jemni on 10/17/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Matched Geometry Effect/Matched_Geometry_EffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Matched_Geometry_EffectApp.swift
+//  Matched Geometry Effect
+//
+//  Created by Adam Jemni on 10/17/21.
+//
+
+import SwiftUI
+
+@main
+struct Matched_Geometry_EffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
